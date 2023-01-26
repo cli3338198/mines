5fff4dcacccc0766f785f7f4ab32b366e88f94b9
Christopher Lim <cli3338198@gmail.com>
2023-01-26 15:41:07 -0800

    refactored function to separate ui and listeners

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
 /******************************************************************************/
+// DOM selectors.
 
-const body = document.querySelector("body") as HTMLElement;
+const target = document.getElementById("target") as HTMLDivElement;
+const board = document.getElementById("board") as HTMLTableElement;
+const newGameButton = document.getElementById(
+  "newGameButton"
+) as HTMLButtonElement;
 
 /******************************************************************************/
 // Typing.
@@ -15,7 +20,7 @@ enum ECellValue {
   six = 6,
   seven = 7,
   eight = 8,
-  bomb = 9,
+  mine = 9,
 }
 
 enum ECellState {
@@ -24,9 +29,9 @@ enum ECellState {
   flagged,
 }
 
-enum ECellDisplay {
-  flag = "🚩",
-  bomb = "💣",
+interface ICancellableClick {
+  click: Promise<unknown>;
+  cancel: () => void;
 }
 
 /******************************************************************************/
@@ -49,46 +54,57 @@ class Cell {
 }
 
 class Game {
-  public static readonly bombs: number = 20;
-  public static readonly rows: number = 16;
-  public static readonly cols: number = 16;
-  public board: Cell[][];
-  public gameOver: boolean;
+  private static readonly _mines: number = 20;
+  private static readonly _rows: number = 16;
+  private static readonly _cols: number = 16;
 
-  constructor() {
+  public board: Cell[][];
+  public gameStarted: boolean;
+  public gameEnded: boolean;
+
+  constructor(
+    private mines: number = Game._mines,
+    private rows: number = Game._rows,
+    private cols: number = Game._cols
+  ) {
+    this.mines = mines;
+    this.rows = rows;
+    this.cols = cols;
     this.board = this.generateBoard();
-    this.gameOver = false;
+    this.gameStarted = true;
+    this.gameEnded = false;
+    // FIXME: use PROXY to log all changes in game to a display
+    // also use proxy for setters so any changes in bombs, dimensions, etc
+    // is logged to display!!
   }
 
   /* Generate a board. */
   private generateBoard(): Cell[][] {
-    const board: Cell[][] = [];
+    // build a board
+    const board: Cell[][] = Array(this.rows)
+      .fill(0)
+      .map(() =>
+        Array(this.cols)
+          .fill(0)
+          .map(() => new Cell())
+      );
 
-    /* Build board. */
-    for (let row = 0; row < Game.rows; row++) {
-      const newRow: Cell[] = [];
-      for (let col = 0; col < Game.cols; col++) {
-        newRow.push(new Cell());
-      }
-      board.push(newRow);
-    }
-
-    /* Place bombs. */
-    let bombs = Game.bombs;
-    while (bombs > 0) {
-      const row = Math.floor(Math.random() * Game.rows);
-      const col = Math.floor(Math.random() * Game.cols);
+    // place mines
+    let placedMines = this.mines;
+    while (placedMines > 0) {
+      const row = Math.floor(Math.random() * this.rows);
+      const col = Math.floor(Math.random() * this.cols);
       const currentCell = board[row][col];
-      if (currentCell.value !== ECellValue.bomb) {
-        currentCell.value = ECellValue.bomb;
-        bombs--;
+      if (currentCell.value !== ECellValue.mine) {
+        currentCell.value = ECellValue.mine;
+        placedMines--;
       }
     }
 
-    /* Place numbers. */
-    for (let row = 0; row < Game.rows; row++) {
-      for (let col = 0; col < Game.cols; col++) {
-        if (board[row][col].value === ECellValue.bomb) {
+    // place numbers
+    for (let row = 0; row < this.rows; row++) {
+      for (let col = 0; col < this.cols; col++) {
+        if (board[row][col].value === ECellValue.mine) {
           this.increment(row + 1, col, board);
           this.increment(row - 1, col, board);
           this.increment(row, col + 1, board);
@@ -104,29 +120,32 @@ class Game {
     return board;
   }
 
-  /* Increment cell value based on # of surrounding bombs. */
+  /* Increment cell value based on # of surrounding mines. */
   private increment(row: number, col: number, board: Cell[][]): void {
     if (
       row in board &&
       col in board[row] &&
-      board[row][col].value !== ECellValue.bomb
+      board[row][col].value !== ECellValue.mine
     ) {
       board[row][col].value++;
     }
   }
 
-  /* Flood fill. */
+  /* Reveal cells. */
+  public reveal(row: number, col: number): void {}
+
+  /* Reveals cells. */
   public flood(value: ECellValue, row: number, col: number): void {
     if (
       !(row in this.board) ||
       !(col in this.board[row]) ||
       this.board[row][col].state === ECellState.visible ||
       this.board[row][col].state === ECellState.flagged ||
-      this.board[row][col].value === ECellValue.bomb ||
+      this.board[row][col].value === ECellValue.mine ||
       this.board[row][col].value === value
     ) {
       return;
-    }
+    } // FIXME: ENDED here!!!!
 
     const cell = this.board[row][col];
     cell.state = ECellState.visible;
@@ -137,15 +156,15 @@ class Game {
     this.flood(value, row, col - 1);
   }
 
-  /* Reveal bombs. */
-  public revealBombs(): void {
-    for (let row = 0; row < Game.rows; row++) {
-      for (let col = 0; col < Game.cols; col++) {
+  /* Reveal mines. */
+  public revealmines(): void {
+    for (let row = 0; row < this.rows; row++) {
+      for (let col = 0; col < this.cols; col++) {
         const cell = this.board[row][col];
         if (
           (cell.state === ECellState.notvisible ||
             cell.state === ECellState.flagged) &&
-          cell.value === ECellValue.bomb
+          cell.value === ECellValue.mine
         ) {
           cell.state = ECellState.visible;
         }
@@ -153,44 +172,9 @@ class Game {
     }
   }
 
-  /* Handle cell click. */
-  public clickCell(id: string): boolean {
-    const [row, col] = id.split(" ");
-    const cell = this.board[Number(row)][Number(col)];
-
-    // if flagged, return
-    if (cell.state === ECellState.flagged) return this.gameOver;
-    // if visible, return
-    if (cell.state === ECellState.visible) return this.gameOver;
-
-    // flood fill
-    if (cell.state === ECellState.notvisible) {
-      cell.state = ECellState.visible;
-
-      this.flood(cell.value, Number(row) + 1, Number(col));
-      this.flood(cell.value, Number(row) - 1, Number(col));
-      this.flood(cell.value, Number(row), Number(col) + 1);
-      this.flood(cell.value, Number(row), Number(col) - 1);
-    }
-
-    // if bomb, set game over
-    if (cell.value === ECellValue.bomb) {
-      this.gameOver = true;
-      this.revealBombs();
-    }
-
-    // if has won, set game over
-    if (this.hasWon()) {
-      this.gameOver = true;
-    }
-
-    return this.gameOver;
-  }
-
-  /* Handle flag cell. */
-  public flagCell(id: string): void {
-    const [row, col] = id.split(" ");
-    const cell = this.board[Number(row)][Number(col)];
+  /* Flag cell. */
+  public flagCell(row: number, col: number): void {
+    const cell = this.board[row][col];
 
     // toggle cell flagged
     if (cell.state === ECellState.flagged) {
@@ -202,40 +186,35 @@ class Game {
 
   /* Check if player has won. */
   public hasWon(): boolean {
-    let bombsFound = 0;
+    let minesFound = 0;
 
-    for (let row = 0; row < Game.rows; row++) {
-      for (let col = 0; col < Game.cols; col++) {
+    for (let row = 0; row < this.rows; row++) {
+      for (let col = 0; col < this.cols; col++) {
         const cell = this.board[row][col];
         if (
           (cell.state === ECellState.flagged ||
             cell.state === ECellState.notvisible) &&
-          cell.value === ECellValue.bomb
+          cell.value === ECellValue.mine
         ) {
-          bombsFound++;
+          minesFound++;
         }
       }
     }
 
-    return bombsFound === Game.bombs;
+    return minesFound === this.mines;
   }
 }
 
 /******************************************************************************/
 // Game logic.
 
+let pendingClicks: ICancellableClick[] = [];
 let live = false;
 let endGame = false;
+let game: Game | null;
 
-createDOMBoard();
-const g = new Game();
-g.board.forEach((row, r) =>
-  row.forEach((cell, c) => {
-    const id = `${r} ${c}`;
-    const domCell = document.getElementById(id) as HTMLTableCellElement;
-    domCell.innerText = String(cell.value);
-  })
-);
+game = new Game();
+createBoard();
 
 /* Start the game. */
 
@@ -246,47 +225,114 @@ g.board.forEach((row, r) =>
 /* Set won. */
 
 /******************************************************************************/
-// DOM manipulation.
+// Event listeners.
 
-/* Create DOM board. */
-function createDOMBoard(): void {
-  const table = document.createElement("table");
-  table.id = "board";
+/* Disable context menu. */
+document.addEventListener("contextmenu", (evt: MouseEvent) => {
+  evt.preventDefault();
+});
 
-  for (let row = 0; row < Game.rows; row++) {
-    const tableRow = document.createElement("tr");
+/* Single click handler. */
+function singleClickHandler(evt: MouseEvent): Promise<void> {
+  // clear pending clicks
+  clearPendingClicks();
+  // create cancellable click with a delay of 300ms
+  const waitForClick = cancellableClick(delay(300));
+  // add the new click
+  addPendingClick(waitForClick);
 
-    for (let col = 0; col < Game.cols; col++) {
-      const tableCell = document.createElement("td");
-      /* Assign id to tableCell. */
-      tableCell.id = `${row} ${col}`;
-      /* Toggle cell visibility. */
-      // toggleCellVisibility(tableCell); TODO: fix this here
-      /* Add click handler. */
-      tableCell.addEventListener("click", tableCellClickHandler);
-      tableCell.addEventListener("contextmenu", tableCellContextMenuHandler);
-
-      tableRow.appendChild(tableCell);
-    }
-    table.appendChild(tableRow);
-  }
-  body.appendChild(table);
+  return waitForClick.click
+    .then(() => {
+      // remove the click
+      removePendingClick(waitForClick);
+      // execute single click
+      const tableCell = evt.target as HTMLTableCellElement;
+      const [row, col] = tableCell.id.split(" ");
+      // reveal cell
+      game?.reveal(Number(row), Number(col));
+      // update ui
+      updateUI();
+      console.log("single clicked", evt.target);
+    })
+    .catch(() => {
+      // remove the click
+      removePendingClick(waitForClick);
+      // handle errors
+      // FIXME:
+    });
 }
 
-/* Flood fill. */
-function flood(value: string, row: number, col: number): void {
-  const id = `${row} ${col}`;
-  const cell = document.getElementById(id) as HTMLTableCellElement;
+/* Double click handler. */
+function doubleClickHandler(evt: MouseEvent): void {
+  // clear pending clicks
+  clearPendingClicks();
+  // execute double click
+  const tableCell = evt.target as HTMLTableCellElement;
+  const [row, col] = tableCell.id.split(" ");
+  // flag cell
+  game?.flagCell(Number(row), Number(col));
+  // update ui
+  updateUI();
+  console.log("double clicked", evt.target);
+}
 
-  if (cell && cell.innerText === value) {
-    toggleCellVisibility(cell);
+/******************************************************************************/
+// UI.
 
-    flood(value, row + 1, col);
-    flood(value, row - 1, col);
-    flood(value, row, col + 1);
-    flood(value, row, col - 1);
+/* Create board. */
+function createBoard() {
+  if (game) {
+    game.board.forEach((row, r) => {
+      const tableRow = document.createElement("tr");
+
+      row.forEach((cell, c) => {
+        const tableCell = document.createElement("td");
+        // assign id to table cell
+        tableCell.id = `${r} ${c}`;
+        // add value to table cell
+        tableCell.innerHTML = String(cell.value);
+        // add attributes to table cell
+        tableCell.setAttribute("data-state", String(cell.state));
+        // add single click handler
+        tableCell.addEventListener("click", singleClickHandler);
+        // add double click handler
+        tableCell.addEventListener("dblclick", doubleClickHandler);
+
+        tableRow.appendChild(tableCell);
+      });
+
+      board.appendChild(tableRow);
+    });
   }
 }
+
+/* Update UI. */
+function updateUI(): void {
+  if (game) {
+    game.board.forEach((row, r) => {
+      row.forEach((cell, c) => {
+        const id = `${r} ${c}`;
+        const tableCell = document.getElementById(id) as HTMLTableCellElement;
+        tableCell.setAttribute("data-state", String(cell.state));
+      });
+    });
+  }
+}
+
+// /* Flood fill. */
+// function flood(value: string, row: number, col: number): void {
+//   const id = `${row} ${col}`;
+//   const cell = document.getElementById(id) as HTMLTableCellElement;
+
+//   if (cell && cell.innerText === value) {
+//     toggleCellVisibility(cell);
+
+//     flood(value, row + 1, col);
+//     flood(value, row - 1, col);
+//     flood(value, row, col + 1);
+//     flood(value, row, col - 1);
+//   }
+// }
 
 /* Toggle cell visibility. */
 function toggleCellVisibility(cell: HTMLTableCellElement): void {
@@ -294,28 +340,75 @@ function toggleCellVisibility(cell: HTMLTableCellElement): void {
 }
 
 /* Cell click handler. */
-function tableCellClickHandler(evt: MouseEvent): void {
-  const cell = evt.target as HTMLTableElement;
-  const id = cell.getAttribute("id") as string;
-  const [row, col] = id.split(" ");
-  const value = cell.innerText;
+// function tableCellClickHandler(evt: MouseEvent): void {
+//   const cell = evt.target as HTMLTableElement;
+//   const id = cell.getAttribute("id") as string;
+//   const [row, col] = id.split(" ");
+//   const value = cell.innerText;
 
-  // if bomb
-  if (value === String(ECellValue.bomb)) {
-    return cell.classList.add("bomb");
-  }
+//   // if bomb
+//   if (value === String(ECellValue.mine)) {
+//     return cell.classList.add("bomb");
+//   }
 
-  flood(value, Number(row), Number(col));
-}
+//   flood(value, Number(row), Number(col));
+// }
 
 /* Context menu handler. */
-function tableCellContextMenuHandler(evt: MouseEvent): void {
-  evt.preventDefault();
-  // FIXME
-  const cell = evt.target as HTMLTableElement;
+// function tableCellContextMenuHandler(evt: MouseEvent): void {
+//   evt.preventDefault();
+//   // FIXME
+//   const cell = evt.target as HTMLTableElement;
+//   cell.classList.toggle("flagged");
+// }
+
+/* Toggle flagged status of cell. */
+function toggleFlagCell(cell: HTMLTableCellElement): void {
   cell.classList.toggle("flagged");
 }
 
 /******************************************************************************/
+// Utils.
+
+/* Create a cancellable click. */
+function cancellableClick(click: Promise<void>): ICancellableClick {
+  let isCancelled = false;
+
+  const wrappedClick = new Promise((resolve, reject) => {
+    click.then(
+      (value: void) => {
+        isCancelled === true ? reject({ isCancelled, value }) : resolve(value);
+      },
+      (error: Error) => reject({ isCancelled, error })
+    );
+  });
+
+  return {
+    click: wrappedClick,
+    cancel: () => {
+      isCancelled = true;
+    },
+  };
+}
+
+/* Add a pending click. */
+function addPendingClick(click: ICancellableClick): void {
+  pendingClicks = [...pendingClicks, click];
+}
+
+/* Remove a pending click. */
+function removePendingClick(click: ICancellableClick): void {
+  pendingClicks = pendingClicks.filter((_click) => _click !== click);
+}
+
+/* Clear pending clicks. */
+function clearPendingClicks(): void {
+  pendingClicks.map((pendingClick) => pendingClick.cancel());
+}
+
+/* Delay for click. */
+function delay(n: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, n));
+}
 
 /******************************************************************************/
